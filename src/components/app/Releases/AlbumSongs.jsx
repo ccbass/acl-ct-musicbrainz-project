5e676cd1a947dda5d396a1676e78ecbf708e72df
Cierra-McDonald <cierralauren15@gmail.com>
2021-05-26 11:37:58 -0700

    refactored AlbumSongs container

--- src/components/app/Releases/AlbumSongs.jsx
+++ src/components/app/Releases/AlbumSongs.jsx
@@ -2,29 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Songs from './Songs';
 import styles from './Releases.css';
+import { fetchRecordings } from '../../services/MusicBrainzApi';
+
 
 const AlbumSongs = () => {
     const [recordings, setRecordings] = useState([]);
     const [loading, setLoading] = useState(true);
     const { release } = useParams();
-
-    const fetchRecordings = async (release) => {
-        const res = await fetch(
-            `http://musicbrainz.org/ws/2/recording?release=${release}&fmt=json`
-        );
-        // fetch('http://musicbrainz.org/ws/2/recording?release=7d166a44-cfb5-4b08-aacb-6863bbe677d6&fmt=json');
-
-        const recs = await res.json();
-
-        return recs.recordings.map((recording) => ({
-            title: recording.title,
-            releaseDate: recording.first_release_date,
-            disambiguation: recording.disambiguation,
-            length: recording.length,
-            id: recording.id,
-            video: recording.video,
-        }));
-    };
 
     useEffect(() => {
         fetchRecordings(release)
