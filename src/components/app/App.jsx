0ee707cda15f45b7c113e72a575884a195332ef7
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-05-26 09:39:33 -0700

    album page working

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import SearchContainer from '../../containers/SearchContainer';
 
+import AlbumContainer from '../../containers/AlbumContainer';
+
 
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        {/* <Route exact path="/" component={SearchContainer}/> */}
-        {/* <Route path="/:artist" component={}/> */}
+        <Route exact path="/" component={SearchContainer}/> 
+        <Route path="/:artist" component={AlbumContainer}/> 
         {/* <Route path="/:artist/:release" component={}/> */}
         {/* <Route path="/:artist/:release/:song" component={}/> */}
       </Switch>
