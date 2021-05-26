6aa1480064ed2b7a7300e6444bd63dc91f20695a
Dylan White <dylan.whitej@gmail.com>
2021-05-25 17:33:21 -0700

    lyric page working

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LyricContainer from '../../containers/LyricContainer';
 
 import SearchContainer from '../../containers/SearchContainer';
 
-
-
 export default function App() {
-  return (
-    <Router>
-      <Switch>
-        {/* <Route exact path="/" component={SearchContainer}/> */}
-        {/* <Route path="/:artist" component={}/> */}
-        {/* <Route path="/:artist/:release" component={}/> */}
-        {/* <Route path="/:artist/:release/:song" component={}/> */}
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                {/* <Route exact path="/" component={SearchContainer}/> */}
+                {/* <Route path="/:artist" component={}/> */}
+                {/* <Route path="/:artist/:release" component={}/> */}
+                <Route
+                    path="/:artist/:release/:song"
+                    component={LyricContainer}
+                />
+            </Switch>
+        </Router>
+    );
 }
