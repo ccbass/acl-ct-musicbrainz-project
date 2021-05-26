c0bf52fe2eb2130c9ed10a2a413bc40ef8869d12
Dylan White <dylan.whitej@gmail.com>
2021-05-26 09:42:40 -0700

    pre merge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -9,6 +8,5 @@ const Header = () => {
         </header>
     );
 };
-
 
 export default Header;
